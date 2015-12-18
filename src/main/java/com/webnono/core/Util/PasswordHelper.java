732494a32ed0ab9a908d79b1b13b5f7e7250d0a7
webnono <1925413267@qq.com>
2015-12-18 11:34:33 +0800

    密码加密工具类+salt

---
+++ src/main/java/com/webnono/core/Util/PasswordHelper.java
@@ -0,0 +1,57 @@
+package com.webnono.core.Util;
+
+import com.webnono.web.model.User;
+import org.apache.shiro.crypto.RandomNumberGenerator;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2015/12/9.
+ */
+@Service
+public class PasswordHelper {
+
+    private RandomNumberGenerator randomNumberGenerator = new SecureRandomNumberGenerator();
+
+
+    private String algorithmName = "md5";
+
+    private int hashIterations = 2;
+
+    public void setRandomNumberGenerator(RandomNumberGenerator randomNumberGenerator) {
+        this.randomNumberGenerator = randomNumberGenerator;
+    }
+
+    public void setAlgorithmName(String algorithmName) {
+        this.algorithmName = algorithmName;
+    }
+
+    public void setHashIterations(int hashIterations) {
+        this.hashIterations = hashIterations;
+    }
+
+    public void encryptPassword(User user) {
+
+
+        user.setSalt(randomNumberGenerator.nextBytes().toHex());
+//        if(user.getSalt() == null)user.setSalt("8d78869f470951332959580424d4bf4f");
+
+        String newPassword = new SimpleHash(
+                algorithmName,
+                user.getPassword(),
+                ByteSource.Util.bytes(user.getSalt()),
+                hashIterations).toHex();
+
+        user.setPassword(newPassword);
+    }
+
+    public static void main(String[] args){
+        User user = new User("admin","123456");
+        PasswordHelper passwordHelper = new PasswordHelper();
+        passwordHelper.encryptPassword(user);
+        System.out.println(user.getPassword());
+        System.out.println(user.getSalt());
+    }
+}
